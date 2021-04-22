c0cf3e6b902ce58a4188bd5b4060c4c85bf4cad9
llennairek <llennairek@gmail.com>
2021-04-22 11:20:32 +0200

    added trip sans fonts

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,15 @@ const wrapper = document.querySelector(".divert .carousel-wrapper");
 const item = wrapper.querySelector(".carousel-item");
 const items = wrapper.querySelectorAll(".carousel-item");
 
+const restWrapper = document.querySelector(".rest .carousel-wrapper");
+const eatWrapper = document.querySelector(".eat .carousel-wrapper");
+
 let left = 0;
 const width = item.offsetWidth;
 const wrapperWidth = wrapper.offsetWidth;
 
-const forward = () => {
+const forward = (e) => {
+  // console.log(e.target);
   const temp = left;
   left -= 2 * (width + 16);
   if (left < (width + 16) * -(items.length - 2)) {
